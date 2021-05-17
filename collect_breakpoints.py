aa37fb304504ae0476638e0ad70fc9303b3a7a8c
Austin Wang <austinwang@Austins-MBP>
2021-05-16 18:18:14 -0700

    collect breakpoints

---
+++ collect_breakpoints.py
@@ -0,0 +1,46 @@
+import pickle
+
+def parse_paf(paf_path, q_cutoff):
+    seqs = {}
+    lens = {}
+    with open(paf_path, "r") as paf_file:
+        for line in paf_file:
+            cols = line.strip.split()
+            qual = int(line[11])
+            if qual < q_cutoff or qual == 255:
+                continue
+
+            qname = line[0]
+            qlen, qstart, qend = int(line[1]), int(line[2]), int(line[3])
+            strand = line[4]
+            rname = line[5]
+            rstart, rend = int(line[6]), int(line[7])
+
+            seq_data = (qstart, qend, strand, rname, rstart, rend)
+            seqs.setdefault(qname, []).append(seq_data)
+
+            lens[qname] = qlen
+
+    return seqs, lens
+
+def get_breakpoints(paf_path, out_path, q_cutoff):
+    seqs, lens = parse_paf(paf_path, q_cutoff)
+    seqs_filtered = {}
+    lens_filtered = {}
+    for k, v in seqs.items():
+        if len(v) >= 2:
+            s = sorted(v)
+            print(s) ####
+            seqs_filtered[k] = s
+            lens_filtered[k] = lens[k]
+
+    res = (seqs_filtered, lens_filtered)
+    with open(out_path, "wb") as out_file:
+        pickle.dump(res, out_file)
+
+if __name__ == "__main__":
+    data_dir = "/oak/stanford/groups/wjg/atwang/scdna/data"
+    paf_path = 
+    out_path = 
+
+    q_cutoff = 50
