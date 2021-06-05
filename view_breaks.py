336e8ce4433d6266dcc4ed5b0632151066de42a7
Austin Wang <austinwang@Austins-MBP>
2021-06-05 11:06:12 -0500

    collect breakpoints

--- view_breaks.py
+++ view_breaks.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     data_dir = "/oak/stanford/groups/wjg/atwang/ecdna/data"
     in_path = os.path.join(data_dir, "COLO320DM_gDNA_nanopore_guppy_4.4_breaks_proc.pickle")
 
-    results_dir = "/oak/stanford/groups/wjg/atwang/ecdna/data"
+    results_dir = "/oak/stanford/groups/wjg/atwang/ecdna/results"
     os.makedirs(results_dir, exist_ok=True)
 
     min_supports = [2]
