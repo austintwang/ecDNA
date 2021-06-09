797a3fc89a0cd08c6a842c73a2e3d6eed1e7a105
Austin Wang <austinwang@Austins-MBP>
2021-06-08 20:38:54 -0500

    plot graph

--- view_graph_coarse.py
+++ view_graph_coarse.py
@@ -78,7 +78,7 @@ def view_graph_coarse(in_path, results_dir):
     plot_dir = os.path.join(results_dir, "graph")
     os.makedirs(plot_dir, exist_ok=True)
     for ind, c in enumerate(components):
-        if c <= 1:
+        if len(c) <= 1:
             continue
         print(len(c)) ####
         result_path = os.path.join(results_dir, f"coarse_{ind:02d}.svg")
